efbbbb3e84c89b247251737d6e586d9356451355
soyfercarvajal <soyfercarvajal@gmail.com>
2022-09-16 03:16:10 -0500

    Ejecicio con bucle while

--- src/Acceso_Aplicacion.java
+++ src/Acceso_Aplicacion.java
@@ -1,5 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class Acceso_Aplicacion {
     public static void main(String[] args) {
-        
+
+        String clave = "Fernando";
+        String pass = "";
+
+        while (clave.equals(pass) == false) {
+            pass = JOptionPane.showInputDialog("Introduce la contraseña por favor");
+
+            if (clave.equals(pass) == false) {
+
+                JOptionPane.showMessageDialog(null, "Contraseña incorrecta");
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Contraseña correcta. \n Acceso permitido");
     }
 }
