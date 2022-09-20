2e7002d798e52030141704c4df674025a1652529
soyfercarvajal <soyfercarvajal@gmail.com>
2022-09-19 19:50:33 -0500

    Ejercicio bucle for

---
+++ src/Comprueba_Mail2.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class Comprueba_Mail2 {
+    public static void main(String[] args) {
+
+        int arroba = 0;
+        boolean punto = true;
+
+        String mail = JOptionPane.showInputDialog("Introduce tu email");
+
+        for (int i = 0; i < mail.length(); i++) {
+            if (mail.charAt(i) == '@') {
+                arroba++;
+            }
+            if (mail.charAt(i) == '.') {
+                punto = true;
+            }
+        }
+        if (arroba == 1 && punto == true) {
+            JOptionPane.showMessageDialog(null, "Es correcto");
+        } else {
+            JOptionPane.showMessageDialog(null, "No es correcto");
+
+        }
+    }
+}
